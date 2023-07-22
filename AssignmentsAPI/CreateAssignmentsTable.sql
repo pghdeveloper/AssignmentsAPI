7dac6861315f97cb50c5754c058b7d8aaeda74eb
pghdeveloper <34989427+pghdeveloper@users.noreply.github.com>
2023-07-22 12:34:34 -0400

    Getting SqlLite going

---
+++ AssignmentsAPI/CreateAssignmentsTable.sql
@@ -0,0 +1,14 @@
+-- Create Table Assignments(
+-- Id TEXT PRIMARY KEY DEFAULT (UUID()),
+-- Assignee TEXT NOT NULL CHECK(length(Assignee) <= 255),
+-- DueDate TEXT NOT NULL,
+-- Description TEXT NOT NULL Check(length(Description) <= 500),
+-- PercentComplete NUMERIC NOT NULL CHECK(PercentComplete == ROUND(PercentComplete, 2)),
+-- IsPriority INTEGER NOT NULL);
+
+--Insert Into Assignments(Id, Assignee, DueDate, Description, PercentComplete, IsPriority)
+--Values(UUID(), 'John Cena', '2023-09-01', 'Test Description', 0, 1);
+
+--DROP Table Assignments;
+
+select * from Assignments;
