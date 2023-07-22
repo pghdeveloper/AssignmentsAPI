94a104259fde02d729850a921a20e7cb4526350c
pghdeveloper <34989427+pghdeveloper@users.noreply.github.com>
2023-07-22 17:54:43 -0400

    Made IsPriority and PercentComplete Null in the database table

--- AssignmentsAPI/CreateAssignmentsTable.sql
+++ AssignmentsAPI/CreateAssignmentsTable.sql
@@ -3,12 +3,14 @@
 -- Assignee TEXT NOT NULL CHECK(length(Assignee) <= 255),
 -- DueDate TEXT NOT NULL,
 -- Description TEXT NOT NULL Check(length(Description) <= 500),
--- PercentComplete NUMERIC NOT NULL CHECK(PercentComplete == ROUND(PercentComplete, 2)),
--- IsPriority INTEGER NOT NULL);
+-- PercentComplete NUMERIC NULL CHECK(PercentComplete == ROUND(PercentComplete, 2)),
+-- IsPriority INTEGER NULL);
 
 -- Insert Into Assignments(Id, Assignee, DueDate, Description, PercentComplete, IsPriority)
--- Values(UUID(), 'Matt Riddle', '2023-09-01', 'Test Description', 0, 1);
+-- Values(UUID(), 'Matt Riddle', '2023-09-01', 'Test Description', 56.76, 1);
+-- Insert Into Assignments(Id, Assignee, DueDate, Description)
+-- Values(UUID(), 'John Cena', '2023-09-01', 'Test Description');
 
 --DROP Table Assignments;
 
-select * from Assignments;
+--select * from Assignments;
---
+++ AssignmentsAPI/Services/AssignmentsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AssignmentsAPI.Services
+{
+	public class AssignmentsService
+	{
+		public AssignmentsService()
+		{
+		}
+	}
+}
+
