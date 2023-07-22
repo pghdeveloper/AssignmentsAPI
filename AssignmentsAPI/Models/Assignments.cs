2df0e4e083fb34c3c4a5ccf5a6f42f6937433786
pghdeveloper <34989427+pghdeveloper@users.noreply.github.com>
2023-07-22 15:59:48 -0400

    Added SqlConnection Factory

---
+++ AssignmentsAPI/Models/Assignments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AssignmentsAPI.Models
+{
+	public class Assignments
+	{
+		public Guid Id { get; set; }
+		public string Assignee { get; set; }
+		public string DueDate { get; set; }
+		public string Description { get; set; }
+		public decimal PercentCompleted { get; set; }
+		public bool IsPriority { get; set; }
+	}
+}
+
---
+++ AssignmentsAPI/Repositories/SqlConnectionFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data.Common;
+
+namespace AssignmentsAPI.Repositories 
+{
+	public class SqlConnectionFactory : ISqlConnectionFactory
+    {
+		public string ConnectionString { get; }
+
+		public SqlConnectionFactory(IConfiguration configuration)
+		{
+			ConnectionString = configuration.GetConnectionString("WebApiDatabase");
+		}
+
+		public DbConnection CreateSqlConnection()
+		{
+			return new SqliteConnection(ConnectionString);
+		}
+
+	}
+
+	public interface ISqlConnectionFactory
+	{
+		string ConnectionString { get; }
+		DbConnection CreateSqlConnection();
+	}
+}
+
